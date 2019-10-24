bf99a8d866e96db6aa2d961321f54b4bc3ecc1c4
Jesse Schooling <jjs175@zips.uakron.edu>
2019-10-24 13:51:58 -0400

    Updating my files

---
+++ TestCode/CollisionScript.py
@@ -0,0 +1,20 @@
+import sys
+import numpy
+import math
+import CollisionHeader.py
+
+def main():
+
+    intake values from Tyler
+    collisioncheckscript for cueball collisions (only run x times based on strikestrength)
+        collision, run collisionscript, check x
+            return cuedatatype and objectdatatype
+            sollisioncheckscript again
+        wall, run wallscript
+            return cuedatatype
+            collisioncheckscript
+        pocket, send scratch warning
+    send final package to Sarah with shipscript
+    
+
+sys.exit (main())
