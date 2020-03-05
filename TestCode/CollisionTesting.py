2794a8a1a18f0905d40bba1777e12c23d242ac9c
Jesse Schooling <jjs175@zips.uakron.edu>
2020-03-05 14:43:28 -0500

    Implemented wall bounce angle controlled test

--- TestCode/CollisionTesting.py
+++ TestCode/CollisionTesting.py
@@ -5,6 +5,7 @@ import math
 exampleCueBall = (1, 0, 59.5, [15,15], math.pi/4)
 exampleObjBall = (1, 1, 59.5, [29,30])
 newColliderCoord = []
+cornerCoords = [100, 100]
 
 # Create a triangle to model resultant vectors
 deltx = exampleCueBall[3][0] - exampleObjBall[3][0]
@@ -34,3 +35,16 @@ else: #Aimed directly at the center of the object ball
 #newColliderCoord[2] = exampleCueBall[3][1] + exampleCueBall[2]*math.sin(thetaH)
 
 print(newColliderThetaf, newCollideeThetaf)
+
+if (0 < newColliderThetaf and newColliderThetaf < (math.pi/2)): #Aimed at top right corner of the table
+    deltxtr = cornerCoords[1] - exampleCueBall[3][0] #Triangle height to top right pocket
+    deltytr = cornerCoords[0] - exampleCueBall[3][1] #Triangle base length to top right pocket
+    hypot = math.sqrt( (math.pow(deltxtr,2) - math.pow(deltytr,2) ) ) #Distance formula measuring hypotenuse to top right pocket
+    theta1 = math.atan(deltxtr/deltytr) #angle from cue ball to tr pocket
+    if (newColliderThetaf < theta1): #Hitting the right wall
+        newColliderThetaf = math.pi - newColliderThetaf
+    elif (newColliderThetaf > theta1): #Hitting the top wall
+        newColliderThetaf = (-1) * newColliderThetaf
+    else: #Gonna scratch it, THROW IN WARNING?
+        pass
+print(newColliderThetaf)
